e4e53ae328979abd2726a4ee0cfa1ac2e8e07d4f
davepm <davidmendoza911119@gmail.com>
2024-09-06 16:13:37 -0600

    fix update dto

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,6 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 import { IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {
   @IsString()
@@ -8,5 +9,6 @@ export class UpdateProductDto extends PartialType(CreateProductDto) {
   name?: string;
   @IsNumber()
   @IsOptional()
+  @Type(() => Number)
   quantity?: number;
 }
